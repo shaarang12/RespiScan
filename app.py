6aed3e931eb78dd11b39bcfe1a5a9f4f77ee9ae8
shaarang.pd <84028033+shaarang12@users.noreply.github.com>
2023-08-10 21:12:20 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import pickle
+import numpy as np
+
+model = pickle.load(open('model.pkl', 'rb'))
+
+def predict_placement(cgpa, iq, profile_score):
+    result = model.predict(np.array([cgpa, iq, profile_score]).reshape(1, 3))
+
+    if result[0] == 1:
+        return 'placed'
+    else:
+        return 'not placed'
+
+def main():
+    st.title("Placement Predictor")
+
+    st.write("Enter the following details:")
+    
+    cgpa = st.number_input("CGPA", min_value=0.0, max_value=10.0, step=0.01)
+    iq = st.number_input("IQ", min_value=0, max_value=200, step=1)
+    profile_score = st.number_input("Profile Score", min_value=0, max_value=100, step=1)
+
+    if st.button("Predict Placement"):
+        result = predict_placement(cgpa, iq, profile_score)
+        st.write(f"The predicted placement result is: {result}")
+
+if __name__ == "__main__":
+    main()
