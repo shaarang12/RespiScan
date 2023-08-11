3da5cee2842ef541865ce77f4bc0689c41f09b7f
shaarang.pd <84028033+shaarang12@users.noreply.github.com>
2023-08-11 13:29:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import numpy as np
 
 # Load the models
 LR_model = pickle.load(open('modelLR.pkl', 'rb'))
-DT_model = pickle.load(open('modelDT.pkl', 'rb'))
+#DT_model = pickle.load(open('modelDT.pkl', 'rb'))
 KNN_model = pickle.load(open('modelKNN.pkl', 'rb'))
 GNB_model = pickle.load(open('modelGNB.pkl', 'rb'))
 # MNB_model = pickle.load(open('modelMNB.pkl', 'rb'))
@@ -118,7 +118,7 @@ def main():
              allergy, wheezing, alcohol, coughing, shortness_of_breath, swallowing_difficulty, chest_pain, anxyf]]
 
     # Make predictions
-    models = [LR_model, DT_model, KNN_model, GNB_model]
+    models = [LR_model, KNN_model, GNB_model]
 
     if st.button("Predict"):
         predictions = [model.predict(data)[0] for model in models]
